e4f12d375c88231d2da71861a2430540e3ce3a4f
JesseJohn7 <143970775+JesseJohn7@users.noreply.github.com>
2024-05-05 19:01:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,195 @@
+/* "use strict" */
+/* alert("Hello");
+alert(2+3);
+
+typeof("Jesse");
+
+
+var myName = "Kevin";
+alert(myName);
+
+var yourName = prompt("what is your name");  
+
+alert( `my name is ${myName} welcome to Javascript ${yourName} `); 
+// alert( "my name is " + myName +", welcome to Javascript " + yourName + "!" ); */
+
+/* 
+var myName="Jesse";
+alert (myName);
+
+
+var yourName = prompt ("What is your name?");
+alert( `my name is ${yourName}`); */
+
+/* var a = 3;
+var b = 8;
+
+console.log("a is " + a);
+console.log("b is " + b); */
+
+
+
+/* function test() {
+    var a = "3";
+    var b = "8"; */
+    
+/***********Do not change the code above 👆*******/
+//Write your code on lines 7 - 9:
+
+
+    
+/***********Do not change the code below 👇*******/
+
+/*     console.log("a is " + a);
+    console.log("b is " + b);
+}
+
+""
+alert("Hello" + " " + "World")
+
+
+var message = "Hello";
+var name = prompt("what's your name?")
+alert( message +  " there, " + name );
+ */
+
+/* let name = "Abraham";
+console.log(name.length);
+
+var yourText = prompt("Please input your words");
+console.log(yourText.length);
+
+
+var tweet = prompt ("compose your tweet");
+var tweetcount = tweet.length;
+alert ("You have written " + tweetcount + " character you have " + (100 - tweetcount) + " characters remaining");
+
+var nAme = "Endrick"
+alert = (nAme.slice(0,1));
+ */
+
+/* var n = Math.random();
+n = n*6;
+n = math.floor(n);
+console.log(n);
+ */
+
+
+
+
+
+/* 
+var nAme= prompt("what is your name");
+alert = ("Good evening" + nAme + "What can we do for you?");
+ */
+/* 
+var tweet = prompt("Compose your tweet");
+alert("You have written" +  tweet.length + "character,you have " + (140-tweet.length) + "characters remaining" );
+var under140 = prompt ("Compose your tweet ");
+alert (under140.slice(0,140)); */
+
+
+/* var name = "Angela";
+alert(name.slice(0,3) ); */
+
+/* a/* */ /* lert (prompt("Whats on your mind").slice(0,140));
+
+var name = prompt("What is your name");
+name = name.toUpperCase();
+alert("Hello" +  name + "you didnt capitalize your text"); */
+ 
+/* function getMilk( bottles ){
+
+    alert ("Go right");
+    alert ("Go left");
+    alert ("Go left");
+    alert ("Go up");
+    alert ("Buy " + bottles + "  bottles of milk")
+    alert ("Go right");
+    alert ("Go right");
+    alert ("Go right");
+} */
+
+/* getMilk(12); */
+
+
+
+/* 
+function getMilk (bottles){
+    var cost = bottles * 1.5;
+} */
+
+    
+
+/* Life in weeks calculator */
+
+/* function lifeInWeeks(age){
+
+    var yearsRemaining = 90-age;
+    var days = yearsRemaining * 365;
+    var weeks = yearsRemaining * 52;
+    var months  = yearsRemaining * 12;
+    var hours = yearsRemaining * 24;
+    var minutes = yearsRemaining * 60;
+    var seconds  = yearsRemaining * 60;
+    
+    console.log("You have "  + days + " days " + weeks + " weeks " + months + " months " + hours + " hours " + minutes + " minutes" + seconds +  "seconds");
+}
+
+
+
+
+lifeInWeeks(20);
+ */
+/*  */
+
+
+
+
+/* var n = Math.random();
+n = Math.floor(n*6)+1
+console.log(n);  */
+
+
+
+var firstName = prompt("please input your name?");
+var secondName = prompt("please input Lover's name?");
+ var lovePercent = Math.floor(Math.random() * 100); 
+var lovePercent = Math.random() * 100;
+lovePercent = Math.floor(lovePercent) + 1;
+console.log(lovePercent + "%")
+alert( firstName + " and "  + secondName  + " your compatability is " + lovePercent + "%");
+
+
+ 
+/*  if (lovePercent > 70) {
+
+    alert(" your compatbility is " + lovePercent + " % " + "your love is as strong as as diamond ");
+}
+
+    else{
+        alert(" your compatability is " + lovePercent + " % " );
+
+
+    }  */
+
+/*     if(lovePercent !== 50){
+
+        alert(" Your fucking amazing bro never give up");
+    }
+        else{
+            alert("Never give up ");
+        } */
+    
+
+    if(lovePercent  >30 && lovePercent <=90){
+    alert("Yipee Your love is as strong as  a rock!");
+    }
+
+     else{ 
+    alert("Aww snap your are not comaptible for each other!");
+    }
+/*     for(var i=1; i<5; i++){
+
+        console.log(i);
+    } */ 
